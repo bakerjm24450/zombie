28121e92b3b31246c981719c4f471c402fbdcb5a
Mac Baker <bakerjm24450@gmail.com>
2018-11-25 17:37:15 -0500

    Added all body parts

--- zombie.py
+++ zombie.py
@@ -1,11 +1,11 @@
 """This is the escape room Zombie prop
 """
 
-from nfci2c import Nfci2c
 import array
-from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
 import time
 import atexit
+from Adafruit_MotorHAT import Adafruit_MotorHAT, Adafruit_DCMotor
+from nfci2c import Nfci2c
 
 class BodyPart(object):
     """A single zombie body part (arm, leg, torso, head, etc)
@@ -73,6 +73,7 @@ class BodyPart(object):
             # did we find the one we're looking for?
             if foundTag == self.tag:
                 # found it, so turn on the magnet
+		self.magnet.setSpeed(224)
                 self.magnet.run(Adafruit_MotorHAT.FORWARD)
 
                 self.on = True
@@ -95,22 +96,102 @@ class BodyPart(object):
 def main():
     """Look for body parts"""
 
-    rightArm = BodyPart(name='Right Arm', 
+    head = BodyPart(name='Head', 
                        tag=array.array('B', [167, 75, 126, 242]),
-                       muxAddr=0x74, muxChannel=7, 
+                       muxAddr=0x74, muxChannel=2, 
+                       motorAddr=0x62, motorId=3)
+
+    headPrevState = False
+
+    rightArm = BodyPart(name='Right Arm', 
+                       tag=array.array('B', [55, 167, 128, 242]),
+                       muxAddr=0x74, muxChannel=4, 
+                       motorAddr=0x62, motorId=2)
+
+    rightArmPrevState = False
+
+    leftArm = BodyPart(name='Left Arm', 
+                       tag=array.array('B', [71, 208, 126, 242]),
+                       muxAddr=0x74, muxChannel=3, 
                        motorAddr=0x62, motorId=1)
 
-    rightPrevState = False
+    leftArmPrevState = False
+
+    chest = BodyPart(name='Chest', 
+                       tag=array.array('B', [231, 206, 126, 242]),
+                       muxAddr=0x74, muxChannel=1, 
+                       motorAddr=0x61, motorId=4)
+
+    chestPrevState = False
+
+    rightLeg = BodyPart(name='Right Leg', 
+                       tag=array.array('B', [151, 207, 126, 242]),
+                       muxAddr=0x74, muxChannel=7, 
+                       motorAddr=0x61, motorId=1)
+
+    rightLegPrevState = False
+
+    leftLeg = BodyPart(name='Left Leg', 
+                       tag=array.array('B', [7, 76, 126, 242]),
+                       muxAddr=0x74, muxChannel=6, 
+                       motorAddr=0x61, motorId=2)
+
+    leftLegPrevState = False
 
     while True:
+        state = head.update()
+
+        if state != headPrevState:
+            headPrevState = state
+            if state:
+                print 'Found the Head'
+            else:
+                print "I've had worse"
+
         state = rightArm.update()
 
-        if state != rightPrevState:
-            rightPrevState = state
+        if state != rightArmPrevState:
+            rightArmPrevState = state
             if state:
-                print('Found the right arm')
+                print 'Found the right arm'
             else:
-                print("I've had worse")
+                print "I've had worse"
+
+        state = leftArm.update()
+
+        if state != leftArmPrevState:
+            leftArmPrevState = state
+            if state:
+                print 'Found the left arm'
+            else:
+                print "I've had worse"
+
+        state = chest.update()
+
+        if state != chestPrevState:
+            chestPrevState = state
+            if state:
+                print 'Found the chest'
+            else:
+                print "I've had worse"
+
+        state = rightLeg.update()
+
+        if state != rightLegPrevState:
+            rightLegPrevState = state
+            if state:
+                print 'Found the right leg'
+            else:
+                print "I've had worse"
+
+        state = leftLeg.update()
+
+        if state != leftLegPrevState:
+            leftLegPrevState = state
+            if state:
+                print 'Found the left leg'
+            else:
+                print "I've had worse"
 
 if __name__ == '__main__':
     main()
